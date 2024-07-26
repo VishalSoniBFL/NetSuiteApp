bfca7486f9f567b7af994ba5a522bee894e3a31c
Vishal Soni <vishal.soni@theblueflamelabs.com>
2024-07-26 17:17:39 +0530

    initail

--- src/NVServer.js
+++ src/NVServer.js
@@ -2,34 +2,26 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const NVServer = () => {
-  const [data, setData] = useState(null);
+  const [responseData, setResponseData] = useState("Taking Response");
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/");
-        setData(response.data);
+        const response = await axios.get("https://td2929815.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=816&deploy=1&compid=TD2929815&ns-at=AAEJ7tMQtd8m8NnU25YV8Jz3fcFKvt4-oCjKMQquAduzp0ahnc0");
+        setResponseData(response.data);
       } catch (error) {
         setError(error.message);
+        console.error("Error making request:", error.message);
       }
     };
 
     fetchData();
   }, []);
 
-  if (error) {
-    return <div>Error fetching data: {error}</div>;
-  }
-
-  if (!data) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
-      <h1>Data from NetSuite</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>{error ? `Error: ${error}` : `Response: ${JSON.stringify(responseData)}`}</h1>
     </div>
   );
 };
