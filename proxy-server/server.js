d588d073199eb92e58272e6f029fa8d9fcbd2040
Vishal Soni <vishal.soni@theblueflamelabs.com>
2024-07-26 15:38:33 +0530

    initail

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors'); // Import cors
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors"); // Import cors
 const app = express();
 const port = 5000; // Ensure this port is not in use
 
@@ -8,23 +8,23 @@ const port = 5000; // Ensure this port is not in use
 app.use(cors());
 
 // Define the route to handle your API request
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const config = {
-    method: 'get',
-    url: 'https://td2929815.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=816&deploy=1&compid=TD2929815&ns-at=AAEJ7tMQtd8m8NnU25YV8Jz3fcFKvt4-oCjKMQquAduzp0ahnc0',
+    method: "get",
+    url: "https://td2929815.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=816&deploy=1&compid=TD2929815&ns-at=AAEJ7tMQtd8m8NnU25YV8Jz3fcFKvt4-oCjKMQquAduzp0ahnc0",
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
-    maxRedirects: 5
+    maxRedirects: 5,
   };
 
   try {
     const response = await axios.request(config);
     res.send(response.data); // Send raw data
   } catch (error) {
-    console.error('Error making request:', error.message);
-    res.status(500).send('Error fetching data from NetSuite');
+    console.error("Error making request:", error.message);
+    res.status(500).send("Error fetching data from NetSuite");
   }
 });
 
